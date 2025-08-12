badee0978dba67fb28dfeec8978f23183dbf39a1
Zohair Laabane Tlemcani <zohair.laabane@outlook.com>
2025-08-12 13:42:23 +0100

    feat: add UI for the meal page

--- app/(protected)/meals/page.tsx
+++ app/(protected)/meals/page.tsx
@@ -1,12 +1,49 @@
-export default function Page() {
+import {columns, Payment} from "./columns"
+import {DataTable} from "./data-table"
+
+async function getData(): Promise<Payment[]> {
+    // Fetch data from your API here.
+    return [
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "m@example.com",
+        },
+        // ...
+    ]
+}
+
+export default async function Page() {
+    const data = await getData()
+
     return (
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-            <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                <div className="bg-muted/50 aspect-video rounded-xl" />
-                <div className="bg-muted/50 aspect-video rounded-xl" />
-                <div className="bg-muted/50 aspect-video rounded-xl" />
+        <div className={"flex min-h-min flex-1 flex-col p-4"}>
+            <div className={"mb-2 flex items-baseline justify-between gap-2"}>
+                <div>
+                    <h2 className={"text-2xl font-bold tracking-tight"}>Meals</h2>
+                    <p className={"text-muted-foreground"}>Manage your meals</p>
+                </div>
+                <div className="flex flex-wrap justify-end gap-2">
+                    <button
+                        className="inline-flex items-center justify-center gap-2 whitespace-nowrap
+                         rounded-md text-sm transition-colors focus-visible:outline-hidden focus-visible:ring-1
+                          focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50
+                           [&amp;_svg]:pointer-events-none [&amp;_svg]:size-4 [&amp;_svg]:shrink-0
+                            bg-green-900 text-primary-foreground shadow-sm hover:bg-green-800 h-9
+                             px-4 py-2 font-semibold">Create
+                        Task
+                    </button>
+                </div>
             </div>
-            <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+            <div className="flex-1">
+                <div className="space-y-4">
+                    <DataTable
+                        columns={columns}
+                        data={data}
+                    />
+                </div>
+            </div>
         </div>
     )
 }
