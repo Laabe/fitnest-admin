aaf7da9384833f3e5344e3967b9d4696aeaa1ccb
Zohair Laabane Tlemcani <zohair.laabane@outlook.com>
2025-10-01 13:26:38 +0100

    refactor: user type

--- types/user.d.ts
+++ types/user.d.ts
@@ -1,5 +1,12 @@
 export interface User {
-    id?: string,
+    id: string,
+    name: string,
+    email: string,
+    avatar?: string,
+    role: string,
+}
+
+export interface UserPayload {
     name: string,
     email: string,
     avatar?: string,
