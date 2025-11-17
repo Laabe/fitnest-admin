d74f66f2c4bd89b59bc850d922e72d90b7a8a93d
Zohair Laabane Tlemcani <zohair.laabane@outlook.com>
2025-11-17 20:49:47 +0100

    feat: add function to generate a SKU

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const generateSKU = (name: string): string => {
+    const hash = Array.from(name).reduce((acc, char) => acc + char.charCodeAt(0), 0);
+    const part1 = (hash * 17 % 10000).toString().padStart(4, "0");
+    const part2 = (hash * 41 % 10000).toString().padStart(4, "0");
+    return `SKU-${part1}-${part2}`;
+}
